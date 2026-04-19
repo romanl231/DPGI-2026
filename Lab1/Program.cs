c1b637eb7eadb51b77c298b310ea884f21500707
Roman <lugovijroman39@gmail.com>
2026-04-19 21:25:26 +0300

    add readme

---
+++ Lab1/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab1;
+
+public class Program
+{
+    /// <summary>
+    /// Entry point of the program.
+    /// </summary>
+    public static void Main(string[] args) => Console.WriteLine("Hello, World!");
+}
